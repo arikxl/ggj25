955b8a27e38b37fc14bfd6965906278b622a5975
arikxl <arikxl@gmail.com>
2025-01-25 14:25:34 +0200

    credits

---
+++ src/components/Credits.tsx
@@ -0,0 +1,13 @@
+
+
+const Credits = () => {
+  return (
+      <footer className="credits">
+          <a href="https://www.linkedin.com/in/arik-alexandrov/" target="_blank">Arik Alexandrov</a>&nbsp;|&nbsp;
+          <a href="https://www.youtube.com/watch?v=V9hav4QPSeU" target="_blank">💪ISRAEL 2025 </a>&nbsp;|&nbsp;
+          <a href="https://github.com/arikxl" target="_blank">arikxl</a> 
+    </footer>
+  )
+}
+
+export default Credits
--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Credits from '../components/Credits';
 
 type Props = {
     setIsGameOn: React.Dispatch<React.SetStateAction<boolean>>;
@@ -7,20 +8,22 @@ type Props = {
 const Start = ({ setIsGameOn }: Props) => {
 
 
-    const coverVideo ='https://res.cloudinary.com/arikxl/video/upload/v1737801046/Ella2023/dunkl1us7nnomuxgjg3y.mp4'
+    const coverVideo = 'https://res.cloudinary.com/arikxl/video/upload/v1737801046/Ella2023/dunkl1us7nnomuxgjg3y.mp4'
 
 
     return (
-        <main className="start">
-            <video className="background-video" autoPlay loop muted>
-                <source src={coverVideo} type="video/mp4" />
-            </video>
-            <section className="content">
-                <p>Choose what to do with the bubbles</p>
-                <button className='answer' style={{padding:'20px 40px'}}
-                    onClick={() => setIsGameOn(true)}>Let's Pop</button>
-            </section>
-        </main>
+            <main className="start">
+                <video className="background-video" autoPlay loop muted>
+                    <source src={coverVideo} type="video/mp4" />
+                </video>
+                <section className="content">
+                    <p>Choose what to do with the bubbles</p>
+                    <button className='answer' style={{ padding: '20px 40px' }}
+                        onClick={() => setIsGameOn(true)}>Let's Pop</button>
+                </section>
+
+                <Credits />
+            </main>
     )
 }
 
